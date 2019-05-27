b7791f8a496f94ea766471be046bf1b1c9b7cef4
Meng Sun <sunmeg.89@gmail.com>
2019-05-27 20:28:43 +0900

    1D quantum well

--- 1D_qw/make_plot.m
+++ 1D_qw/make_plot.m
@@ -8,6 +8,6 @@ load result
 fig1 = figure();
 for i = 1:5
 hold on
-plot(x_coord,eig_v(i)+100*(abs(eig_mode(:,i).^2)));
+plot(x_coord,(abs(eig_mode(:,i).^2)));
 hold off
 end
--- 1D_qw/partial.m
+++ 1D_qw/partial.m
@@ -3,12 +3,13 @@ function [ partial_x, partial_xx ] = partial( Nx,dx )
 
 e1 = ones(Nx,1);
 partial_x = spdiags([e1,-1*e1],[1,-1],Nx,Nx)/(2*dx);
-partial_x(1,:) = 0;     % boundary condition
-partial_x(end,:) = 0;   % boundary condition
+% partial_x(1,:) = 0;     % boundary condition
+% partial_x(end,:) = 0;   % boundary condition
 
 partial_xx = spdiags([e1,e1,-2*e1],[1,-1,0],Nx,Nx)/(dx*dx);    % partial operator partial_xx;
-partial_xx(1,:) = 0;    % periodic boundary condition;
-partial_xx(end,:) = 0; % periodic boundary condition;
-
+% partial_xx(1,:) = 0;    % periodic boundary condition;
+% partial_xx(end,:) = 0; % periodic boundary condition;
+% partial_xx(:,1) = 0;
+% partial_xx(:,end) = 0;
 end
 
